f4e420abafe1098ad900d73ce6b8876dda50038e
codalo <ckodalo@gmail.com>
2022-08-18 05:36:24 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function ([]) {
+    return (['Antonia', 'Nuru']);
+}
+const returnLastTwoDrivers = function ([]) {
+    return (['Amari', 'Mo']);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(multiple) {
+    return function (fare) {
+       return fare * multiple;
+    }
+} 
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers([], drivers) {
+    return drivers([]);
+}
